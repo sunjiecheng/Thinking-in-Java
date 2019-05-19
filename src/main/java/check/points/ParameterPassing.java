1d35ea163c33fa2755252ceb32103361da42c8f8
jiecheng <tojiecheng@163.com>
2019-05-19 16:40:00 +0800

    按值传递考点例子提交

--- src/main/java/check/points/ParameterPassing.java
+++ src/main/java/check/points/ParameterPassing.java
@@ -6,15 +6,44 @@ package check.points;
  */
 public class ParameterPassing {
     public static void main(String[] args) {
-        // 基本数据类型
+        // 1、传入基本数据类型
         int num = 5;
         System.out.println(num);
         changeValue(num);
         System.out.println(num);
+
+        // 2、传入引用数据类型
+        Person person = new Person();
+        //先显示这个对象的name属性
+        System.out.println(person.name);
+
+        /*changeNameByObject(person);
+        //再显示这个对象的name属性，看是否发生了变化
+        System.out.println(person.name);*/
+
+        changeName(person);
+        //再显示这个对象的name属性，看是否发生了变化
+        System.out.println(person.name);
+
     }
 
     //定义了一个改变参数值的函数
     public static void changeValue(int x) {
-        x = x *2;
+        x = x * 2;
     }
+
+    //定义一个改变对象属性的方法
+    public static void changeNameByObject(Person p) {
+        p.name = "Rose";
+    }
+
+    //定义一个改变对象属性的方法
+    public static void changeName(Person p) {
+        p = new Person();
+        p.name = "Rose";
+    }
+}
+
+class Person {
+    public String name = "Jack";
 }
