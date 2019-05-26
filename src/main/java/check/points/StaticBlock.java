d8d7c30af14507bd61504080feff02bc0733f92e
jiecheng <tojiecheng@163.com>
2019-05-26 18:21:47 +0800

    提交第五章

---
+++ src/main/java/check/points/StaticBlock.java
@@ -0,0 +1,22 @@
+package check.points;
+
+/**
+ * @author jiecheng
+ * @create 2019-05-26 下午5:44
+ */
+public class StaticBlock {
+    public StaticBlock() {//构造函数
+        System.out.println("A的构造函数");
+    }
+
+    {//构造代码块
+        System.out.println("A的构造代码块");
+    }
+
+    static {//静态代码块
+        System.out.println("A的静态代码块");
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/sjc/initialization/Spiciness.java
@@ -0,0 +1,6 @@
+//: enumerated/Spiciness.java
+package com.sjc.initialization;
+
+public enum Spiciness {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+} ///:~
