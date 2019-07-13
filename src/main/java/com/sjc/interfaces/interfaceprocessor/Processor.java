6002ba497652f84a9da0663eb1e71b5b376b0211
jiecheng <tojiecheng@163.com>
2019-07-13 12:26:38 +0800

    提交第9章

---
+++ src/main/java/com/sjc/interfaces/interfaceprocessor/Processor.java
@@ -0,0 +1,8 @@
+//: interfaces/interfaceprocessor/Processor.java
+package com.sjc.interfaces.interfaceprocessor;
+
+public interface Processor {
+    String name();
+
+    Object process(Object input);
+} ///:~
